48161a0ae9b9addf12152b9b0b8d1bd47f688fd4
gabriel <gabrielcmoris@gmail.com>
2022-07-06 16:04:43 +0200

    add first component without exporting

---
+++ pages/FirstRoute.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="w-full h-full bg-green-300 p-8">
+        <h1 class="font-bold text-green-600 text-lg">I am another page!</h1>
+    </div>
+    <FirstComponent/>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,5 @@
     <div class="w-full bg-gray-300 p-8">
         <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
     </div>
+    <FirstComponent/>
 </template>
