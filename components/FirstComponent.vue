d79aea093061e5c6b01c66f7e8012a8f1671ad1f
gabriel <gabrielcmoris@gmail.com>
2022-07-06 16:35:33 +0200

    add route

--- components/FirstComponent.vue
+++ components/FirstComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full bg-blue-300 p-8">
-        <h1 class="font-bold text-blue-600 text-lg">I am a Component!</h1>
-    </div>
+        <div class="w-full bg-blue-300 p-8">
+            <h1 class="font-bold text-blue-600 text-lg">I am a Component!</h1>
+        </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+    <NuxtLink to="/firstroute">First Route</NuxtLink>
     <div class="w-full bg-gray-300 p-8">
         <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
     </div>
